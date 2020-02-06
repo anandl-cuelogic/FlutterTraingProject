981a76f9a52db727c1c3849d86043c81ee611927
anandl-cuelogic <anand.lilhare@cuelogic.com>
2020-02-06 12:13:45 +0530

    Material Designe Basic -Text and Widget Properties

--- flutter_first_1/lib/main.dart
+++ flutter_first_1/lib/main.dart
@@ -3,16 +3,19 @@ import 'package:flutter/material.dart';
 void main() => runApp(HomeApp());
 
 class HomeApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Center (
-
-      child: Text("Hello Flutter",
-      textDirection: TextDirection.ltr, style: TextStyle(fontWeight: FontWeight.bold),),
-
+    return Material(
+      color: Colors.lightBlue,
+      child: Center(
+          child: Text(
+        "Hello, This is my first program",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 25.05,
+            fontStyle: FontStyle.italic),
+      )),
     );
-
-
   }
 }
