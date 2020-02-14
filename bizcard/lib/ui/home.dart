79cee4572810bca192250455274f3d37657653a3
anandl-cuelogic <anand.lilhare@cuelogic.com>
2020-02-14 18:20:13 +0530

    Completing Section 14

--- bizcard/lib/ui/home.dart
+++ bizcard/lib/ui/home.dart
@@ -1,10 +1,76 @@
-
-
 import 'package:flutter/material.dart';
 
+class Wisdom extends StatefulWidget {
+  @override
+  _WisdomState createState() => _WisdomState();
+}
 
+class _WisdomState extends State<Wisdom> {
+  List quotes = [
+    "I love you the more in that I believe you had liked me for my own sake and for nothing else.",
+    "But man is not made for defeat. A man can be destroyed but not defeated.",
+    "When you reach the end of your rope, tie a knot in it and hang on.",
+    "There is nothing permanent except change.",
+    "You cannot shake hands with a clenched fist.",
+    "Let us sacrifice our today so that our children can have a better tomorrow.",
+    "Do not mind anything that anyone tells you about anyone else. Judge everyone and everything for yourself."
+  ];
+  int _index = 0;
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: Container(
+                    width: 350,
+                    height: 200,
+                    margin: EdgeInsets.all(30.0),
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(14.5),
+                    ),
+                    child: Center(child: Text(quotes[_index % quotes.length],
+                    style: TextStyle(
+                      color: Colors.grey.shade600,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 16.25
+                    ),
 
+                    ))),
+              ),
+            ),
+            Divider(
+              thickness: 1.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 18.0),
+              child: FlatButton.icon(
+                  onPressed: _showQuote,
+                  color: Colors.greenAccent.shade700,
+                  icon: Icon(Icons.wb_sunny),
+                  label: Text(
+                    "Inspire me!",
+                    style: TextStyle(fontSize: 18.5, color: Colors.white),
+                  )),
+            ),
+            Spacer()
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showQuote() {
+    setState(() {
+      _index += 1;
+    });
+  }
+}
 
 class ScaffoldExample extends StatelessWidget {
   @override
@@ -12,7 +78,6 @@ class ScaffoldExample extends StatelessWidget {
     return Container();
   }
 }
-
 
 class CustomButton extends StatelessWidget {
   @override
@@ -28,72 +93,65 @@ class Home extends StatelessWidget {
   }
 }
 
-
 class BizCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-       appBar: AppBar (
-         title: Text("BizCard"),
-         centerTitle: true,
-       ),
+      appBar: AppBar(
+        title: Text("BizCard"),
+        centerTitle: true,
+      ),
       body: Container(
         alignment: Alignment.center,
         child: Stack(
           alignment: Alignment.topCenter,
-           children: <Widget>[
-             _getCard(),
-             _getAvatar()
-           ],
+          children: <Widget>[_getCard(), _getAvatar()],
         ),
-
       ),
-
-
     );
   }
 
- Container _getCard() {
+  Container _getCard() {
     return Container(
       width: 350,
       height: 200,
       margin: EdgeInsets.all(50.05),
       decoration: BoxDecoration(
-        color: Colors.pinkAccent,
-          borderRadius: BorderRadius.circular(4.5)
+          color: Colors.pinkAccent, borderRadius: BorderRadius.circular(4.5)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Anand Lilhare",
+            style: TextStyle(
+                fontSize: 20.9,
+                color: Colors.white,
+                fontWeight: FontWeight.w500),
+          ),
+          Text("Buildappswithpaulo.com"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.person_outline),
+              Text("T: @anandlilhare")
+            ],
+          )
+        ],
       ),
-     child: Column(
-       mainAxisAlignment: MainAxisAlignment.center,
-       children: <Widget>[
-         Text("Anand Lilhare",
-         style: TextStyle( fontSize:  20.9,
-         color: Colors.white,
-         fontWeight: FontWeight.w500),),
-
-         Text("Buildappswithpaulo.com"),
-         Row(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: <Widget>[
-             Icon(Icons.person_outline),
-             Text("T: @anandlilhare")
-           ],
-         )
-       ],
-     ),
     );
-
   }
 
   Container _getAvatar() {
-    return Container (
+    return Container(
       width: 100,
       height: 100,
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(50.0)) ,
+        borderRadius: BorderRadius.all(Radius.circular(50.0)),
         border: Border.all(color: Colors.redAccent, width: 1.2),
-        image: DecorationImage(image: NetworkImage("https://picsum.photos/300/300"),fit:BoxFit.cover),
+        image: DecorationImage(
+            image: NetworkImage("https://picsum.photos/300/300"),
+            fit: BoxFit.cover),
       ),
     );
   }
