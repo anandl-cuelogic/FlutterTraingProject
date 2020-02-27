1c96a2d937af2b6d10f76b4b2ab26afdc829cb02
anandl-cuelogic <anandl@cuelogic.com>
2020-02-27 09:40:50 +0530

    working on list view

--- bizcard/lib/json_parsing/json_parsing.dart
+++ bizcard/lib/json_parsing/json_parsing.dart
@@ -1,7 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
-
-
 
 class JsonParsingSimple extends StatefulWidget {
   @override
@@ -9,16 +9,50 @@ class JsonParsingSimple extends StatefulWidget {
 }
 
 class _JsonParsingSimpleState extends State<JsonParsingSimple> {
+  Future data;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    data = getData();
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Json Parsing"),
       ),
-      body: Container(
+      body: Center(
+        child: Container (
+          child: FutureBuilder(
+              future: getData(),
+              builder: (context, AsyncSnapshot snpshot){
+                if(snpshot.hasData){
+                  return CreatListView(snpshot,context);
+                }else {
+                  return CircularProgressIndicator();
+                }
 
-      ),
+              }),
+        ),
+      )
     );
+
+  }
+
+  Future getData() async {
+    String url = "https://jsonplaceholder.typicode.com/posts";
+    Future data;
+    Network network = Network(url);
+    data = network.fetchData();
+   /* data.then((value){
+      print(value);
+    });*/
+    return data;
   }
 }
 
@@ -29,13 +63,12 @@ class Network {
 
   Future fetchData() async {
     print("$url");
-    Response  response = await get(Uri.encodeFull(url));
+    Response response = await get(Uri.encodeFull(url));
 
-    if(response?.statusCode == 200){
-       // ok
-      print(response.body)
-      
-      return response.body;
+    if (response?.statusCode == 200) {
+      // ok
+     // print(response.body);
+      return json.decode(response.body);
     } else {
       print(response.statusCode);
     }
