317523a9f5b07d44663dc472665d849218964c39
anandl-cuelogic <anandl@cuelogic.com>
2020-03-03 09:40:23 +0530

    working on data base

--- flutter_firebase_demo/lib/ui/board_app.dart
+++ flutter_firebase_demo/lib/ui/board_app.dart
@@ -8,8 +8,28 @@ class BoardApp extends StatefulWidget {
 }
 
 class _BoardAppState extends State<BoardApp> {
+
+  var firebStoraseDb = Firestore.instance.collection("board").snapshots();
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+       appBar: AppBar(
+         title: Text("Community"),
+       ),
+      body: StreamBuilder(
+          stream:  firebStoraseDb,
+          builder: (context, snapshots){
+            if(!snapshots.hasData) return CircularProgressIndicator();
+
+            return ListView.builder(
+                itemCount: snapshots.data.document.length,
+                itemBuilder: (context, int index){
+                 return Text(snapshots.data.document[index]['title']);
+            });
+
+      }),
+    );
   }
 }
